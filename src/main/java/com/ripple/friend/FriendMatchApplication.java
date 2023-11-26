c61fab815754ba36a3d8c265490d043a6022f655
cloud-ripple <1493440094@qq.com>
2023-11-26 14:58:01 +0800

    整合session和redis实现共享存储

--- src/main/java/com/ripple/friend/FriendMatchApplication.java
+++ src/main/java/com/ripple/friend/FriendMatchApplication.java
@@ -3,7 +3,10 @@ package com.ripple.friend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+//@EnableRedisHttpSession
 @SpringBootApplication
 @MapperScan("com.ripple.friend.mapper")
 public class FriendMatchApplication {
